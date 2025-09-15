7b7cc4572e79a156ad7e399a2584d49bf287f10d
FerasAbukweik <firas.abukweik@gmail.com>
2025-09-15 23:06:03 +0300

    transfer frontend page

--- FrontEnd/Bank/src/app/app.ts
+++ FrontEnd/Bank/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './components/sidebar/sidebar';
 import { Dashboard } from './components/Pages/dashboard/dashboard';
 import { Accounts } from './components/Pages/accounts/accounts';
+import { Transfer } from './components/Pages/transfer/transfer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , Sidebar , Dashboard ,Accounts],
+  imports: [RouterOutlet , Sidebar , Dashboard ,Accounts , Transfer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
